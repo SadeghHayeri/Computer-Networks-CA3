a8cbee842f9e4060ed2fb5f5124624cf33e0d5b4
SadeghHayeri <hayerisadegh@gmail.com>
2018-04-27 18:23:07 +0430

    config added.

---
+++ src/Config.java
@@ -0,0 +1,8 @@
+public class Config {
+    public static final String SERVER_IP = "127.0.0.1";
+    public static final int SERVER_PORT = 9000;
+    public static final int CLIENT_PORT = 9001;
+    public static final int DEFAULT_PAYLOAD_LIMIT_IN_BYTES = 1408;
+    public static final long TIMEOUT = 50;
+    public static final byte[] PhANTON_BYTE = new byte[1];
+}
